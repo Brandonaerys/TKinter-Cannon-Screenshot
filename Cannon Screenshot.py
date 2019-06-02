694696bf81771dda447d59d39254ca2509a40b43
Brandonaerys <35339827+Brandonaerys@users.noreply.github.com>
2019-06-02 11:38:02 +0800

    Update Cannon Screenshot.py

--- Cannon Screenshot.py
+++ Cannon Screenshot.py
@@ -5,56 +5,85 @@ import time
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
 
+class VideoCapture:
+    def __init__(self, video_source):
+        # Open the video source
+        self.vid = cv.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+        # Get video source width and height
+        self.width = self.vid.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
 
 class App:
-    def __init__(self, window, window_title, image_path="background.jpg"):
-        # set video origin
-        self.video = cv.VideoCapture("./Test_RLWBBC_2.mov")
-        print("press 's' for screenshots")
-        time.sleep(1)
+    def __init__(self, window, window_title, video_source):
         # setup tk widget
         self.window = window
         self.window.title(window_title)
-        # play video stream
-        while True:
-            self.ret, self.orig_frame = self.video.read()
-            self.height, self.width = self.orig_frame.shape[:2]
+        self.video_source = video_source
 
-            cv.imshow("frame", self.orig_frame)
-
-            self.key = cv.waitKey(1)
-            if self.key & 0xFF == ord('q'):
-                break
-            elif self.key & 0xFF == ord('s'):
-                self.img =  cv.cvtColor(self.orig_frame, cv.COLOR_BGR2RGB)
-                break
-        self.video.release()
-        # create canvas that fits the screenshot
-        self.canvas = tk.Canvas(self.window, width = self.width, height = self.height)
-        # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
-        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.img))
-        # Add a PhotoImage to the Canvas
-        self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        # bind canvas events
-        self.canvas.bind("<ButtonPress-1>", self.Press)
-        self.canvas.bind("<ButtonRelease-1>", self.Release)
-        self.canvas.bind("<Return>", self.LengthCalculator)
+        self.vid = VideoCapture(self.video_source)
+        self.canvas = tk.Canvas(self.window, width = self.vid.width, height = self.vid.height)
         self.canvas.pack()
+
+
+        self.btn_screenshot=tk.Button(window, text="Screenshot", width=50, command=self.screenshot)
+        self.btn_screenshot.pack(anchor=tk.W, expand=True)
+
+        self.delay = 5
+        self.update()
+
         self.window.mainloop()
+
+
+        # Callback for button
+    def screenshot(self):
+        ret, cap_frame = self.vid.get_frame()
+        self.newTK = tk.Tk()
+        self.picture = tk.Canvas(self.newTK, width = self.vid.width, height = self.vid.height)
+        self.img = cv.cvtColor(cap_frame, cv.COLOR_BGR2RGB)
+        self.pht = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.img))
+        self.picture.create_image(0, 0, image=self.pht, anchor=tk.NW)
+        # bind canvas events
+        self.picture.bind("<ButtonPress-1>", self.Press)
+        self.picture.bind("<ButtonRelease-1>", self.Release)
+        self.picture.bind("<Return>", self.LengthCalculator)
+        self.picture.pack()
+        self.picture.focus_set()
+
         self.Press = [[-1,-1], [-1,-1]]
         self.Release = [[-1,-1], [-1,-1]]
         self.PressCount = 0
-        print(self.PressCount)
         self.ReleaseCount = 0
-        # Callback for the "Blur" button
+
+    def update(self):
+        ret, orig_frame = self.vid.get_frame()
+        if ret:
+            frame = cv.cvtColor(orig_frame, cv.COLOR_BGR2RGB)
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+
+        self.window.after(self.delay, self.update)
 
     def Press(self, event):
-        self.canvas.focus_set()
+        self.picture.focus_set()
         self.Press[self.PressCount][0] = event.x
         self.Press[self.PressCount][1] = event.y
         self.PressCount += 1
     def Release(self, event):
-        self.canvas.focus_set()
+        self.picture.focus_set()
         self.Release[self.ReleaseCount][0] = event.x
         self.Release[self.ReleaseCount][1] = event.y
         self.ReleaseCount += 1
@@ -66,4 +95,4 @@ class App:
         self.hypo = math.sqrt((self.MidOne[0] - self.MidTwo[0]) ** 2 + (self.MidOne[1] - self.MidTwo[1]) ** 2)
         print(self.hypo)
 # Create a window and pass it to the Application object
-Obj = App(tk.Tk(), "Screenshot")
+App(tk.Tk(), "Screenshot", "./Test_RLWBBC_1.MOV")
